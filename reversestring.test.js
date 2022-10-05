22079c0b93da9cce977c2dd6aeeac0cb5fa36146
Usirusen Ikon <lexicon048@gmail.com>
2022-10-05 13:07:14 +0100

    Remove second import function

--- reversestring.test.js
+++ reversestring.test.js
@@ -1,5 +1,4 @@
 const reverseString = require('./reversestring.js');
-const reversedString = require('./reversestring.js');
 
 test('reversestring function should return the reverse of the given string', () => {
   expect(reverseString('runner')).toBe('rennur');
